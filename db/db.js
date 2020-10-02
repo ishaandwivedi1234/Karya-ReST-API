71d1bac07f6d35902d702cdb1ef74ee206ad603b
ishaandwivedi1234 <ishaan.dwivedi99@gmail.com>
2020-10-02 20:24:53 +0530

    bug fix

--- db/db.js
+++ db/db.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
 const winston = require("winston");
 
-module.exports = function () {
+module.exports = async function () {
   try {
-   await mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/karya", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(
+      process.env.MONGODB_URL || "mongodb://localhost/karya",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
     winston.info("Connected to MongoDb");
   } catch (ex) {
     winston.error(ex);
